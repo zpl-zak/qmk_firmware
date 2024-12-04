02bd229bb95a3cd65ba0432fbc55427b545f7ebe
lokher <lokher@gmail.com>
2024-12-04 11:43:40 +0800

    Added K17 Max

---
+++ keyboards/keychron/k17_max/jis_encoder/white/config.h
@@ -0,0 +1,54 @@
+/* Copyright 2024 @ Keychron (https://www.keychron.com)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#ifdef LED_MATRIX_ENABLE
+/* LED Matrix driver configuration */
+#    define LED_MATRIX_LED_COUNT 107
+#    define DRIVER_COUNT 1
+#    define DRIVER_CS_PINS \
+        { B9 }
+
+/* Scan phase of led driver set as MSKPHASE_9CHANNEL(defined as 0x03 in snled27351.h) */
+#    define SNLED27351_PHASE_CHANNEL MSKPHASE_9CHANNEL
+/* Set LED driver current */
+#    define SNLED27351_CURRENT_TUNE \
+        { 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A, 0x2A }
+
+/* Set to infinit, which is use in USB mode by default */
+#    define LED_MATRIX_TIMEOUT LED_MATRIX_TIMEOUT_INFINITE
+/* Allow shutdown of led driver to save power */
+#    define LED_MATRIX_DRIVER_SHUTDOWN_ENABLE
+/* Turn off backlight on low brightness to save power */
+#    define LED_MATRIX_BRIGHTNESS_TURN_OFF_VAL 48
+
+/* Indications */
+#    define DIM_CAPS_LOCK
+#    define CAPS_LOCK_INDEX 56
+#    define DIM_NUM_LOCK
+#    define NUM_LOCK_INDEX 35
+#    define LOW_BAT_IND_INDEX \
+        { 95 }
+#    define BT_HOST_LED_MATRIX_LIST \
+        { 20, 21, 22 }
+#    define P2P4G_HOST_LED_MATRIX_LIST \
+        { 23 }
+#    define BAT_LEVEL_LED_LIST \
+        { 20, 21, 22, 23, 24, 25, 26, 27, 28, 29 }
+
+#    define LED_MATRIX_KEYPRESSES
+#endif
---
+++ keyboards/keychron/k17_max/readme.md
@@ -0,0 +1,35 @@
+# Keychron K17 Max
+
+![Keychron K17 Max](https://cdn.shopify.com/s/files/1/0059/0630/1017/files/K17-Max-3.jpg?v=1720250313)
+
+A customizable 96% low profile keyboard.
+
+* Keyboard Maintainer: [Keychron](https://github.com/keychron)
+* Hardware Supported: Keychron K17 Max
+* Hardware Availability: [Keychron K17 Max QMK/VIA Wireless Custom Mechanical Keyboard](https://www.keychron.com/products/keychron-k17-max-qmk-via-wireless-custom-mechanical-keyboard)
+
+Make example for this keyboard (after setting up your build environment):
+
+    make keychron/k17_max/ansi/rgb:default
+    make keychron/k17_max/ansi/white:default
+
+    make keychron/k17_max/iso/rgb:default
+    make keychron/k17_max/iso/white:default
+
+    make keychron/k17_max/jis/rgb:default
+    make keychron/k17_max/jis/white:default
+
+Flashing example for this keyboard:
+
+    make keychron/k17_max/ansi/rgb:default:flash
+    make keychron/k17_max/ansi/white:default:flash
+
+    make keychron/k17_max/iso/rgb:default:flash
+    make keychron/k17_max/iso/white:default:flash
+
+    make keychron/k17_max/jis/rgb:default:flash
+    make keychron/k17_max/jis/white:default:flash
+
+**Reset Key**: Disconnect the USB cable, toggle mode switch to "Cable", hold down the *Esc* key or reset button underneath space bar, then connect the USB cable.
+
+See the [build environment setup](https://docs.qmk.fm/#/getting_started_build_tools) and the [make instructions](https://docs.qmk.fm/#/getting_started_make_guide) for more information. Brand new to QMK? Start with our [Complete Newbs Guide](https://docs.qmk.fm/#/newbs).
